a63272528acc565f752a3d05243b172c9fb9663f
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-01-18 16:41:48 -0800

    add useEffect to cleanup the number of times the function is run

---
+++ src/components/scratch.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+let myTimer;
+
+const MyComponent = (props) => {
+  const [timerIsActive, setTimerIsActive] = useState(false);
+
+  const { timerDuration } = props; // using destructuring to pull out specific props values
+
+  useEffect(() => {
+    if (!timerIsActive) {
+      setTimerIsActive(true);
+      myTimer = setTimeout(() => {
+        setTimerIsActive(false);
+      }, timerDuration);
+    }
+  }, [timerIsActive, timerDuration]);
+};
